84f5791b836d2cd6ddcfebaf943c6b772a70ad18
翟怡丰 <zhaiyifeng@bytedance.com>
2025-11-17 14:21:55 +0800

    feat: rebase

---
+++ curl_i2i.sh
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+curl -X POST "http://localhost:8188/generate" -H "Content-Type: application/json" -d'{
+    "resize_height": 1224,
+    "resize_width": 1024,
+=======
+curl -X POST "http://localhost:8000/generate" -H "Content-Type: application/json" -d'{
+    "resize_height": 1536,
+    "resize_width": 1536,
+>>>>>>> ef0cd4c826ed89e55af12f8cd667a3bf647fb770
+    "input_image": "/app/OMGSR/my_tests/image_beauty.png",
+    "output_dir": "experiments_omgsr_f",
+    "process_size": 1024,
+    "upscale": 4,
+    "mid_timestep": 244,
+    "align_method": "adain",
+    "guidance_scale": 1.0
+  }'
--- infer/omgsr_f_infer_model.py
+++ infer/omgsr_f_infer_model.py
@@ -94,7 +94,9 @@ def create_gaussian_weight(tile_size, sigma=0.3):
     return gaussian_weight
 
 class OMGSR_F_Infer(torch.nn.Module):
-    def __init__(self, flux_path, lora_path, device, weight_dtype=torch.bfloat16, mid_timestep=295, guidance_scale=1.0):
+    def __init__(self, flux_path, lora_path, device, weight_dtype=torch.bfloat16, mid_timestep=295, guidance_scale=1.0,
+                 compile_policy=False, torch_compile_fullgraph=False, torch_compile_dynamic=True,
+                 torch_compile_mode="default", quantize_policy=False):
         super().__init__()
         vae = AutoencoderKL.from_pretrained(
             flux_path,
@@ -129,6 +131,15 @@ class OMGSR_F_Infer(torch.nn.Module):
         flux_transformer.eval()
         self.vae = vae
         self.flux_transformer = flux_transformer
+        if quantize_policy:
+            from torchao.quantization import Float8DynamicActivationFloat8WeightConfig, quantize_
+            quantize_(self.flux_transformer, Float8DynamicActivationFloat8WeightConfig())
+        if compile_policy:
+            from infer.dynamic_shape_patch import apply_torch_compile_dynamic_shape_monkey_patch
+            apply_torch_compile_dynamic_shape_monkey_patch()
+            self.flux_transformer = torch.compile(self.flux_transformer, mode=torch_compile_mode,
+                                                  fullgraph=torch_compile_fullgraph, dynamic=torch_compile_dynamic)
+            # self.vae = torch.compile(self.vae,mode=torch_compile_mode,fullgraph=torch_compile_fullgraph, dynamic=torch_compile_dynamic)
         self.device = device
         self._init_tiled_vae(encoder_tile_size=1024, decoder_tile_size=224)
 
@@ -188,6 +199,8 @@ class OMGSR_F_Infer(torch.nn.Module):
             width=w,
         )
         # One-Step Predict.
+        print(f"model forward 未分片 单次执行.")
+        start_time = time.time()
         model_pred = self.flux_transformer(
             hidden_states=lq_latent,
             timestep=torch.tensor([self.t_curr], device=lq_latent.device),  # One-Step
@@ -198,6 +211,7 @@ class OMGSR_F_Infer(torch.nn.Module):
             img_ids=latent_image_ids,
             return_dict=False,
         )[0]
+        print(f"model forward 未分片 单次执行 done.阶段耗时为:{(time.time() - start_time):.4f} s")
 
         lq_latent = lq_latent + (self.t_prev - self.t_curr) * model_pred  # 注意
 
@@ -208,7 +222,13 @@ class OMGSR_F_Infer(torch.nn.Module):
             vae_scale_factor=self.vae_scale_factor,
         )
         lq_latent = (lq_latent / self.vae.config.scaling_factor) + self.vae.config.shift_factor
+        start_time = time.time()
         pred_img = self.vae.decode(lq_latent.to(self.vae.dtype), return_dict=False)[0]
+<<<<<<< HEAD
+        print(f"未分片 vae decode done.阶段耗时为:{(time.time() - start_time)} ms")
+=======
+        print(f"未分片 vae decode done.阶段耗时为:{(time.time() - start_time):.4f} ms")
+>>>>>>> ef0cd4c826ed89e55af12f8cd667a3bf647fb770
         return pred_img
 
     def _forward_tile(self, lq_latent, prompt_embeds, pooled_prompt_embeds, text_ids, latent_image_ids, tile_size, tile_overlap):
@@ -231,6 +251,8 @@ class OMGSR_F_Infer(torch.nn.Module):
 
         input_list = []
         noise_preds = []
+        print(f"model forward 多次执行由于图片被分割 {grid_rows}x{grid_cols} tiles.")
+        start_time = time.time()
         for row in range(grid_rows):
             for col in range(grid_cols):
                 if col < grid_cols-1 or row < grid_rows-1:
@@ -286,7 +308,11 @@ class OMGSR_F_Infer(torch.nn.Module):
                     )
                     input_list = []
                 noise_preds.append(model_out)
-
+<<<<<<< HEAD
+        print(f"model forward 多次执行 done.阶段耗时为:{(time.time() - start_time)} s")
+=======
+        print(f"model forward 多次执行 done.阶段耗时为:{(time.time() - start_time):.4f} s")
+>>>>>>> ef0cd4c826ed89e55af12f8cd667a3bf647fb770
         # Stitch noise predictions for all tiles
         noise_pred = torch.zeros(lq_latent.shape, device=lq_latent.device)
         contributors = torch.zeros(lq_latent.shape, device=lq_latent.device)
@@ -313,16 +339,22 @@ class OMGSR_F_Infer(torch.nn.Module):
         # Average overlapping areas with more than 1 contributor
         noise_pred /= contributors
         model_pred = noise_pred
-        lq_latent = lq_latent + (self.t_prev - self.t_curr) * model_pred  
+        lq_latent = lq_latent + (self.t_prev - self.t_curr) * model_pred
         lq_latent = (lq_latent / self.vae.config.scaling_factor) + self.vae.config.shift_factor
+        start_time = time.time()
         pred_img = self.vae.decode(lq_latent.to(self.vae.dtype), return_dict=False)[0]
-
+<<<<<<< HEAD
+        print(f"分片 vae decode done.阶段耗时为:{(time.time() - start_time)} s")
+=======
+        print(f"分片 vae decode done.阶段耗时为:{(time.time() - start_time):.4f} s")
+>>>>>>> ef0cd4c826ed89e55af12f8cd667a3bf647fb770
         return pred_img
 
     def forward(self, lq_img, prompt_embeds, pooled_prompt_embeds, text_ids, latent_image_ids, tile_size, tile_overlap):
         torch.cuda.synchronize()
         start_time = time.time()
         lq_latent = encode_images(lq_img.to(self.vae.dtype), self.vae, self.weight_dtype)
+        print(f"vae encode done.阶段耗时为:{(time.time() - start_time)} s")
         _, _, h, w = lq_latent.shape
         if h * w <= tile_size * tile_size:
             print(f"[Tiled Latent]: the input size is tiny and unnecessary to tile.")
@@ -332,6 +364,5 @@ class OMGSR_F_Infer(torch.nn.Module):
             pred_img = self._forward_tile(lq_latent, prompt_embeds, pooled_prompt_embeds, text_ids, latent_image_ids, tile_size, tile_overlap)
         torch.cuda.synchronize()
         t = time.time() - start_time
-        print(f"Inference time per image: {t}s")
         return pred_img, t
     
